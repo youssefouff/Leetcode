27a115665cefe946175cc860349e1ede361d402a
Youssef Ouf <47798816+youssefouff@users.noreply.github.com>
2022-09-02 19:05:28 +0200

    Time: 55 ms (83.89%), Space: 15.6 MB (15.40%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -2,15 +2,18 @@ import re
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        newString = re.sub("[^a-z|^0-9]","",s.lower())
-        l = 0
-        r = len(newString)-1
+        #convert all upper to lower
+        #remove all non alphanumeric characters
+        newString=re.sub(r'[\W_]+', '', s.lower())
+         
+        l=0
+        r=len(newString) - 1
         while l < r:
             if newString[l] != newString[r]:
-                return False
+                    return False
+            
             l+=1
             r-=1
         return True
-                
                 
         
