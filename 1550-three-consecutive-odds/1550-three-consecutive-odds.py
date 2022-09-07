41760cefc3b82f70c75cc132f8cb72ab1473ef9a
Youssef Ouf <47798816+youssefouff@users.noreply.github.com>
2022-09-07 19:18:59 +0200

    Time: 113 ms (5.05%), Space: 14 MB (18.33%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.py
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.py
@@ -1,20 +1,19 @@
 class Solution:
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
         """
-        left pointer at the first int and right pointer to the second int
+         count number of odds and once I reach count = 3 return true
         
         """
         if len(arr) < 3:
             return False
         count = 0
         for i in range(len(arr)):
-            if arr[i] % 2 == 0:
-                count = 0
-                continue
+            if arr[i] % 2 == 1:
+                count += 1
             else:
-                count+=1
-                if count == 3:
-                    return True
+                count = 0
+            if count == 3:
+                return True
         return False
                 
                 
