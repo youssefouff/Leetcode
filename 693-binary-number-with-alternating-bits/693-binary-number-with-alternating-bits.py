2c174d7a6e70ce7e868106bbac15eb4db117986a
Youssef Ouf <47798816+youssefouff@users.noreply.github.com>
2022-09-07 17:02:30 +0200

    Time: 48 ms (48.15%), Space: 13.9 MB (55.43%) - LeetHub

--- 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.py
+++ 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.py
@@ -1,5 +1,7 @@
 class Solution:
     def hasAlternatingBits(self, n: int) -> bool:
+        if n == 0:
+            return True
         n = bin(n).replace("0b","")
         print(n)
         for i in range(1,(len(n))):
