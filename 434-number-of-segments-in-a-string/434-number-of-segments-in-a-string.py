793219812bc42542623a040c6f76c8212d5fd617
Youssef Ouf <47798816+youssefouff@users.noreply.github.com>
2022-09-07 19:00:21 +0200

    Time: 59 ms (14.26%), Space: 13.9 MB (49.05%) - LeetHub

--- 434-number-of-segments-in-a-string/434-number-of-segments-in-a-string.py
+++ 434-number-of-segments-in-a-string/434-number-of-segments-in-a-string.py
@@ -1,5 +1,10 @@
 class Solution:
     def countSegments(self, s: str) -> int:
+        """
+        if I'm on the first element of the string and it is not space then I start my count
+        if I'm on an char which is space then continue, till I reach a non space character             then check if the prev char was a space the -> counter++
+        
+        """
         count = 0
         for i in range(len(s)):
             if (i == 0 or s[i-1] == " ") and s[i] != " ":
