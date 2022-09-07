863db09de75626a355bb9a4549f1ec8d96db87af
Youssef Ouf <47798816+youssefouff@users.noreply.github.com>
2022-09-07 16:25:44 +0200

    Time: 181 ms (27.92%), Space: 15.6 MB (18.77%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -2,7 +2,8 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         """
         left pointer for a unique integer and right pointer to scan the array
-        if nums[r] = nums[r-1] -> this is a duplicate so r+=1, else nums[l] = nums[r]
+        if nums[r] = nums[r-1] -> this is a duplicate so r+=1, 
+        else nums[l] = nums[r] and l++
     
         """
         l, r = 1, 1
