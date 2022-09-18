8fb9d65ca60756cbd1e035fafc41b7d24df61d18
Youssef Ouf <47798816+youssefouff@users.noreply.github.com>
2022-09-18 23:50:15 +0200

    Time: 323 ms (5.10%), Space: 14.1 MB (89.48%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.py
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -1,8 +1,6 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
         ans = []
-        i=0
-        while i in range (len(nums)):
+        for i in range(len(nums)):
             ans.append(nums[nums[i]])
-            i+=1
         return ans
